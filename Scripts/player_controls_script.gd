0e35eec7a1c251c80643d8db2c890a122d2bddb5
Atticus <callum01eales@gmail.com>
2025-10-20 14:33:19 +0100

    First Push

---
+++ Scripts/player_controls_script.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var SPEED = 400
+@export var ACCEL = 100
+@export var FRCTION = 3
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
+	var target_velocity = input_vector.normalized() * SPEED
+	
+	if input_vector.length() > 0:
+		velocity = velocity.lerp(target_velocity, ACCEL * delta)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, FRCTION * delta)
+		
+	move_and_slide()
