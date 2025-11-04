eff779a16f8c884794b4201711f7890368af4159
Rod <rodsmail22@gmail.com>
2025-11-04 20:50:53 +0200

    portal gun (w/ bug)
    does not snap properly to opposite side

---
+++ Scripts/Portalbullet.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	move_and_slide()
+	pass
+
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var xtemp : float
+	var ytemp : float
+	print($".".name)
+	if body.has_meta("Portalable"):
+		if body.get_meta("Portalable"):
+			xtemp = sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))
+			if xtemp > body.get_child(0).shape.size.x  + 100 :
+				ytemp = body.position.y - (((body.get_child(0).shape.size.x + 100)) * sin(body.rotation)) + 5 * sin(body.rotation - PI / 2)
+				xtemp = body.position.x - (((body.get_child(0).shape.size.x + 100)) * cos(body.rotation)) + 5 * cos(body.rotation - PI / 2)
+			elif xtemp < 100:
+				xtemp = body.position.x - (10 * cos(body.rotation)) + 5 * cos(body.rotation - PI / 2) 
+				ytemp = body.position.y - (10 * sin(body.rotation)) + 5 * sin(body.rotation - PI / 2) 
+			else:
+				ytemp = body.position.y - (((sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))) - 100) * sin(body.rotation)) + 5 * sin(body.rotation - PI / 2)
+				xtemp = body.position.x - (((sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))) - 100) * cos(body.rotation)) + 5 * cos(body.rotation - PI / 2) 
+
+			if $".".get_meta("Leftportal") == true :
+				$"../Leftport".position.x = xtemp
+				$"../Leftport".position.y = ytemp
+				$"../Leftport".rotation = body.rotation
+			else:
+				$"../Rightport".position.y = ytemp
+				$"../Rightport".position.x = xtemp
+				$"../Rightport".rotation = body.rotation
+			
+			
+			
+
+	
+
+	pass # Replace with function body.
--- Scripts/player_controls_script.gd
+++ Scripts/player_controls_script.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@onready var projectile_scene: PackedScene = preload("res://NPCs/Portalbull.tscn")
 @export var SPEED = 400
 @export var ACCEL = 100
 @export var FRCTION = 8
@@ -14,3 +14,26 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.lerp(Vector2.ZERO, FRCTION * delta)
 		
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("RMB"):
+		if projectile_scene == null:
+			return
+		var portbull = projectile_scene.instantiate()
+		get_parent().add_child(portbull)
+		portbull.position.x = $".".position.x + 60 * (get_viewport().get_mouse_position().x - $".".position.x) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		portbull.position.y = $".".position.y+ 60 * (get_viewport().get_mouse_position().y - $".".position.y) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		
+		portbull.velocity.y = 300 * (get_viewport().get_mouse_position().y - $".".position.y) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		portbull.velocity.x = 300 * (get_viewport().get_mouse_position().x - $".".position.x) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		portbull.set_meta("Leftportal",false)
+	elif Input.is_action_just_pressed("LMB"):
+		if projectile_scene == null:
+			return
+		var portbull = projectile_scene.instantiate()
+		get_parent().add_child(portbull)
+		portbull.position.x = $".".position.x + 60 * (get_viewport().get_mouse_position().x - $".".position.x) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		portbull.position.y = $".".position.y+ 60 * (get_viewport().get_mouse_position().y - $".".position.y) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		
+		portbull.velocity.y = 300 * (get_viewport().get_mouse_position().y - $".".position.y) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		portbull.velocity.x = 300 * (get_viewport().get_mouse_position().x - $".".position.x) / sqrt(pow(get_viewport().get_mouse_position().x - $".".position.x,2)+pow(get_viewport().get_mouse_position().y - $".".position.y,2))
+		portbull.set_meta("Leftportal",true)
