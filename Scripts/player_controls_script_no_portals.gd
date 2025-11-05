0f0917f7f6b92c73db6677be2979b020036ef40b
Atticus <callum01eales@gmail.com>
2025-11-05 02:35:46 +0000

    small change to player script
    made a sperate player script as so the work being done on the portals isn't overwritten.
    
    You're welcome.

---
+++ Scripts/player_controls_script_no_portals.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+# Please note this is not a replacement for the player script.
+# I saw that changes were being made to the original player script involing the portals.
+# I made this seperate script to prevent any overrides in the repository.
+
+@export var SPEED = 400
+@export var ACCEL = 100
+@export var FRCTION = 3
+@onready var agent: NavigationAgent2D = $NavigationAgent2D
+@onready var sprite = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
+	var target_velocity = input_vector.normalized() * SPEED
+	
+	if input_vector.length() > 0:
+		velocity = velocity.lerp(target_velocity, ACCEL * delta)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, FRCTION * delta)
+	
+	move_and_slide()
+	_update_animation(input_vector)
+
+func _update_animation(input_vector: Vector2) -> void:
+	if input_vector.length() == 0:
+		sprite.play("Idle")
+		return
+	
+	if abs(input_vector.x) > abs(input_vector.y):
+		sprite.play("Side")
+		sprite.flip_h = input_vector.x > 0
+	else:
+		if input_vector.y > 0:
+			sprite.play("Front")
+		else:
+			sprite.play("Back")
+
+# so, Mort got us into a fucking car wreck
+# and now I can't move it move it anymore!
