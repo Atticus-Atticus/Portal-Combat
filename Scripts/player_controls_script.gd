3511657dc7e93a7094a89715a27e789211a849dc
Atticus <callum01eales@gmail.com>
2025-11-05 02:22:19 +0000

    More work done to the enemies
    some tweaks to their AIs
    added animations
    added weapons

--- Scripts/player_controls_script.gd
+++ Scripts/player_controls_script.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var SPEED = 400
 @export var ACCEL = 100
 @export var FRCTION = 3
+@onready var agent: NavigationAgent2D = $NavigationAgent2D
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	var input_vector = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
@@ -14,6 +16,21 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.lerp(Vector2.ZERO, FRCTION * delta)
 	
 	move_and_slide()
+	_update_animation(input_vector)
+
+func _update_animation(input_vector: Vector2) -> void:
+	if input_vector.length() == 0:
+		sprite.play("Idle")
+		return
+	
+	if abs(input_vector.x) > abs(input_vector.y):
+		sprite.play("Side")
+		sprite.flip_h = input_vector.x > 0
+	else:
+		if input_vector.y > 0:
+			sprite.play("Front")
+		else:
+			sprite.play("Back")
 
 # so, Mort got us into a fucking car wreck
 # and now I can't move it move it anymore!
