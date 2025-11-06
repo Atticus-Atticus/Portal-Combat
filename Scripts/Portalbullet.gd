693704c87f32b41411ab565c40786f7e90cf7f23
Rod <rodsmail22@gmail.com>
2025-11-06 17:51:47 +0200

    fixed the bug, sorry if it breaks things
    you may need to update things sorry

--- Scripts/Portalbullet.gd
+++ Scripts/Portalbullet.gd
@@ -23,15 +23,15 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_meta("Portalable"):
 		if body.get_meta("Portalable"):
 			xtemp = sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))
-			if xtemp > body.get_child(0).shape.size.x  + 100 :
-				ytemp = body.position.y - (((body.get_child(0).shape.size.x + 100)) * sin(body.rotation)) + 5 * sin(body.rotation - PI / 2)
-				xtemp = body.position.x - (((body.get_child(0).shape.size.x + 100)) * cos(body.rotation)) + 5 * cos(body.rotation - PI / 2)
+			if xtemp > body.get_child(0).shape.size.x - 100 :
+				ytemp = body.position.y - (((body.get_child(0).shape.size.x - 200)) * sin(body.rotation)) + 20 * sin(body.rotation - PI / 2)
+				xtemp = body.position.x - (((body.get_child(0).shape.size.x - 200)) * cos(body.rotation)) + 20 * cos(body.rotation - PI / 2)
 			elif xtemp < 100:
-				xtemp = body.position.x - (10 * cos(body.rotation)) + 5 * cos(body.rotation - PI / 2) 
-				ytemp = body.position.y - (10 * sin(body.rotation)) + 5 * sin(body.rotation - PI / 2) 
+				xtemp = body.position.x - (10 * cos(body.rotation)) + 20 * cos(body.rotation - PI / 2) 
+				ytemp = body.position.y - (10 * sin(body.rotation)) + 20 * sin(body.rotation - PI / 2) 
 			else:
-				ytemp = body.position.y - (((sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))) - 100) * sin(body.rotation)) + 5 * sin(body.rotation - PI / 2)
-				xtemp = body.position.x - (((sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))) - 100) * cos(body.rotation)) + 5 * cos(body.rotation - PI / 2) 
+				ytemp = body.position.y - (((sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))) - 100) * sin(body.rotation)) + 20 * sin(body.rotation - PI / 2)
+				xtemp = body.position.x - (((sqrt(pow(body.position.x-$".".position.x,2) + pow(body.position.y-$".".position.y,2))) - 100) * cos(body.rotation)) + 20 * cos(body.rotation - PI / 2) 
 
 			if $".".get_meta("Leftportal") == true :
 				$"../Leftport".position.x = xtemp
